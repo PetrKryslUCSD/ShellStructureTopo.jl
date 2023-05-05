a46200c46b06b9dce7390f1da2b4425ccafb857a
Petr Krysl <pkrysl@ucsd.edu>
2023-05-04 20:24:40 -0700

    fix last test

--- test/test_all.jl
+++ test/test_all.jl
@@ -608,12 +608,13 @@ using Random
 using FinEtools
 using FinEtools.MeshExportModule: VTK, MESH
 using FinEtools.MeshImportModule
+using Main: maybeunzip
 using ShellStructureTopo: make_topo_faces, create_partitions
 using MeshSteward: vtkwrite
 using Metis
 using Test
 function test()
-    output = MeshImportModule.import_ABAQUS(joinpath("./models", "heat_105_5mm.inp"))
+    output = maybeunzip(MeshImportModule.import_ABAQUS, joinpath("../models", "heat_105_5mm.inp"))
     fens, fes = output["fens"], output["fesets"][1]
     bfes = meshboundary(fes)
     surfids, partitionids, surface_elem_per_partition  = create_partitions(fens, bfes, 250; crease_ang = pi/8, cluster_max_normal_deviation = pi/3)
